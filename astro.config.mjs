3fb2e4381051315734e3da2a8bbed9c815bd00b5
Mark Jaquith <mark@jaquith.me>
2022-09-08 02:00:37 -0400

    Deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+	site: 'https://markjaquith.github.io',
+  base: '/wcus-people-quiz',
 });
